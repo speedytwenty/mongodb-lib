22b762c5ea94344b0416c56fa57ceed617c3da9e
speedytwenty <speedytwenty@gmail.com>
2020-05-12 14:34:17 -0600

    Initial commit.

---
+++ index.js
@@ -0,0 +1,9 @@
+const Database = require('./lib/database');
+const Collection = require('./lib/collection');
+const Index = require('./lib/indexing');
+
+module.exports = {
+  Database,
+  Collection,
+  Index,
+};
