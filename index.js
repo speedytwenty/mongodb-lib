27218701dfd4ef32c268dcbc1e1c1b15249806b5
Chris Lee <speedytwenty@gmaio.com>
2021-01-10 00:22:28 -0700

    Initial refactoring. Needs documentation and unit tests.

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-const Database = require('./lib/database');
-const Collection = require('./lib/collection');
-const Index = require('./lib/indexing');
+const nativeConnect = require('mongodb');
+const connectAsync = require('./lib/operations/connect');
+const connectAndInitialize = require('./lib/operations/connectAndInitialize');
 
-module.exports = {
-  Database,
-  Collection,
-  Index,
+nativeConnect.Db = require('./lib/db');
+nativeConnect.Collection = require('./lib/collection');
+
+const connect = (conf, options = {}) => {
+  return new Promise((resolve, reject) => {
+    const { initialize = false } = options;
+    let op = connectAsync;
+    if (conf.collections && initialize) {
+      op = connectAndInitialize;
+    }
+    op(conf)
+      .then((res) => resolve(res))
+      .catch(reject);
+  });
 };
+
+Object.entries(nativeConnect).forEach(([n, v]) => {
+  if (['Db', 'Collection'].indexOf(n) > -1) return;
+  connect[n] = v;
+  connectAsync[n] = v;
+});
+
+connect.connectSync = nativeConnect;
+connect.connectAsync = connectAsync;
+connect.connectAndInitialize = connectAndInitialize;
+
+module.exports = connect;
